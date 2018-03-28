41ceddd4f32db1bc3cc1a145ee68bd3997cc929f
Just Benya <justbenya@gmail.com>
2018-03-29 00:21:23 +0700

    Закончил секцию. Добавил gulp. Большие изменения.

---
+++ gulpfile.js
@@ -0,0 +1,27 @@
+'use strict';
+
+var gulp = require('gulp');
+var sass = require('gulp-sass');
+var browserSync  = require('browser-sync').create();
+
+// Static Server + watching scss/html files
+gulp.task('serve', ['sass'], function () {
+
+  browserSync.init({
+    server: "./"
+  });
+
+  gulp.watch("sass/*/*.scss", ['sass']);
+  gulp.watch("*.html").on('change', browserSync.reload);
+});
+
+// Compile sass into CSS & auto-inject into browsers
+gulp.task('sass', function () {
+  return gulp.src('sass/main.scss')
+    .pipe(sass().on('error', sass.logError))
+    .pipe(gulp.dest("./css"))
+    .pipe(browserSync.stream());
+});
+
+gulp.task('default', ['serve']);
+
